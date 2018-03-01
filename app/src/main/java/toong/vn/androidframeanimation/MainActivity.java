957599200f6cfe661f7a60d352733a88c786d1e9
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-03-01 10:09:34 +0700

    Update strangly case

--- app/src/main/java/toong/vn/androidframeanimation/MainActivity.java
+++ app/src/main/java/toong/vn/androidframeanimation/MainActivity.java
@@ -2,7 +2,6 @@ package toong.vn.androidframeanimation;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageWalker = findViewById(R.id.image_walker);
+        ((AnimationDrawable) imageWalker.getDrawable()).stop();
 
         findViewById(R.id.button_start_frame_animation).setOnClickListener(
                 new View.OnClickListener() {
@@ -23,19 +23,18 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         final AnimationDrawable animationDrawable =
                                 (AnimationDrawable) imageWalker.getDrawable();
-                        if(!animationDrawable.isRunning()) {
-                            int duration = 0;
-                            for(int i = 0; i < animationDrawable.getNumberOfFrames(); i++){
-                                duration += animationDrawable.getDuration(i);
-                            }
-                            animationDrawable.start();
-                            new Handler().postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    animationDrawable.stop();
-                                }
-                            }, duration);
-                        }
+                        animationDrawable.stop();
+                        animationDrawable.start();
+                    }
+                });
+
+        findViewById(R.id.button_stop_frame_animation).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final AnimationDrawable animationDrawable =
+                                (AnimationDrawable) imageWalker.getDrawable();
+                        animationDrawable.stop();
                     }
                 });
     }
